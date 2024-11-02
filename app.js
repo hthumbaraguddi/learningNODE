e848543a08152961f9948415406430d12a7646c5
Harsha Thumbaraguddi <harsha.thumbaraguddi@gmail.com>
2024-11-02 17:03:36 +0530

    Greetings

--- app.js
+++ app.js
@@ -1,32 +1,65 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log("URL:",req.url);
-    console.log("Method:",req.method);
-    console.log("Header:",req.headers);
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
 
-
-    console.log(Date.now());
-    //Sending response Back:
-
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write(
-
-        `
+    if (url === '/') {
+        res.write('<html>');
+        res.write(`
             <head>
-                <title> Let say Hello ! to Node </title>
+                <title>Let send message to NODE</title>
             </head>
             <body>
-                <h1>
-                    Hello From Node.js Server and Now time is ${new Date().toLocaleString()}
-                <h1>
+                <h1>Now time is ${new Date().toLocaleString()}</h1>
+                <form action="/message" method="POST">
+                    Could you please type your Name:<input type="text" name="message">
+                    <button type="submit">Submit</button>
+                </form>
+            </body>
+        `);
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const fullMessage = [];
+        req.on('data', (messageBit) => {
+            fullMessage.push(messageBit);
+        });
+
+        req.on('end', () => {
+            const parsedMessage = Buffer.concat(fullMessage).toString().split('=')[1];
+            console.log(parsedMessage);
+
+            res.write('<html>');
+            res.write(`
+                <head>
+                    <title>Received Message from NODE</title>
+                </head>
+                <body>
+                    <h1>Now time is ${new Date().toLocaleString()}</h1>
+                    <h2>Hello,  ${parsedMessage}, Welcome. </h2>
+                    <a href="/">Go Back</a>
+                </body>
+            `);
+            res.write('</html>');
+            return res.end();
+        });
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write(`
+            <head>
+                <title>Let say Hello! to Node</title>
+            </head>
             <body>
-        `
-    )
-
-    res.write('</html>');
-
-})
+                <h1>Hello from Node.js Server! Now time is ${new Date().toLocaleString()}</h1>
+            </body>
+        `);
+        res.write('</html>');
+        res.end();
+    }
+});
 
 server.listen(4000);
