d0c0d15c3f937fc035a488f441c4abd6fb3348c6
Harsha Thumbaraguddi <harsha.thumbaraguddi@gmail.com>
2024-11-02 16:06:51 +0530

    Sending Response

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log("URL:",req.url);
+    console.log("Method:",req.method);
+    console.log("Header:",req.headers);
+
+
+    console.log(Date.now());
+    //Sending response Back:
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write(
+
+        `
+            <head>
+                <title> Let say Hello ! to Node </title>
+            </head>
+            <body>
+                <h1>
+                    Hello From Node.js Server and Now time is ${new Date().toLocaleString()}
+                <h1>
+            <body>
+        `
+    )
+
+    res.write('</html>');
+
 })
 
 server.listen(4000);
