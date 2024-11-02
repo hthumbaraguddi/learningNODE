a9b454b06bcd4dae12385f1fa17f0b4d70d3c481
Harsha Thumbaraguddi <harsha.thumbaraguddi@gmail.com>
2024-11-02 18:34:48 +0530

    Converting Node app to Express App

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const route = require('./route')
 
-const server = http.createServer(route.GreetingRoute);
+// const server = http.createServer(route.GreetingRoute);
 
-server.listen(4000);
+// server.listen(4000);
+
+app.use(route.GreetingRoute);
+
+app.listen(4000);
