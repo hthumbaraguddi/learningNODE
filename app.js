cf251866fb09ff32bf15123e183ac75a5112a775
Harsha Thumbaraguddi <harsha.thumbaraguddi@gmail.com>
2024-11-02 21:28:28 +0530

    sending and receving message in POST

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-//const http = require('http');
 const express = require('express');
 const bodyParser =  require('body-parser');
-const route = require('./route');
+//const route = require('./route'); // we will not this module. 
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : false}));
 
+//app.use(route.GreetingRoute);
 
-// const server = http.createServer(route.GreetingRoute);
+//lets buidl an app
 
-// server.listen(4000);
+//middleware
+app.use('/add-product',(req,res,next)=>{
+    let formCode=  `<form action="/product" method="POST">
+                    Product Name:<input type="text" name="message">
+                    <button type="submit">Submit</button>
+                    </form>`;
+    res.send(formCode);
+})
 
-app.use(route.GreetingRoute);
+app.use('/product',(req,res,next)=>{
+   
+    res.send(`<h1>Product we received:${req.body.message}, will be added to product catalog soon . . .</h1>`);
+})
+
+
+app.use('/',(req,res,next)=>{
+   
+    res.send(`<h1>Hello...</h1>`);
+})
 
 app.listen(4000);
