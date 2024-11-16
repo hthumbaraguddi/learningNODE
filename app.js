17fe25cd9a6b59aadcf294b6a513f92754850fae
Harsha Thumbaraguddi <harsha.thumbaraguddi@gmail.com>
2024-11-16 16:36:49 +0530

    Styling

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const express = require('express');
 const path = require('path');
-const bodyParser =  require('body-parser');
-const adminRoute = require('./routes/admin');
-const shopRoute = require('./routes/shop');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//No effect on order of these but if you use router.use in the admin route or shoproute then you will redirected to '/' router
-//which will give you hello....
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-//To check above statement, we have modified the route.get to route.use in adminRoute
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
-app.use(shopRoute);
-app.use('/admin',adminRoute);
-
-
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','file-notfound.html'));
-})
-
-
-app.listen(4000);
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,15 @@ const express = require('express');
 
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 
 //router.get('/add-product',(req,res,next)=>{
 router.get('/add-product',(req,res,next)=>{
     
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'..','views','add-product.html'));
 })
 
 
@@ -24,5 +26,7 @@ router.get('/',(req,res,next)=>{
     res.redirect('/');
     
 });
+
+//module.exports = path.dirname(require.main.filename);
 
 module.exports = router;
