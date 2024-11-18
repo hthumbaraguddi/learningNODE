2982f8a1585481ea7d8cc43e827420034bd7ed52
Harsha Thumbaraguddi <harsha.thumbaraguddi@gmail.com>
2024-11-18 23:42:01 +0530

    404 with Products Model

--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require('../controllers/products');
+
 const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productController.getProducts);
 
 module.exports = router;
