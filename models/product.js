ba062add724608ba3b73e49d4c531592d23228f8
Harsha Thumbaraguddi <harsha.thumbaraguddi@gmail.com>
2024-11-19 00:45:33 +0530

    Callback for fetch all. Excellent Callback

--- models/product.js
+++ models/product.js
@@ -1,17 +1,39 @@
-const products =[];
 
-
+const path = require('path');
+const rootDirectory = require('../util/path');
+const fs = require('fs');
+const { error } = require('console');
+const { fileURLToPath } = require('url');
+const fileName = path.join(rootDirectory,'data','products.json');
 module.exports = class Product{
     constructor(t){
         this.title = t;
     }
 
     save(){
-        products.push(this);
+        
+        console.log('rootDirectory:', rootDirectory);
+        
+        fs.readFile(fileName,(error,fileContent)=>{
+            let products =[];
+            if(!error){
+                products =  JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(fileName,JSON.stringify(products),(error)=>{
+                console.log("error while writing product file")
+            })
+        })
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(callBack){
+        fs.readFile(fileName,(error,fileContent)=>{
+            if(error){
+                callBack([]);
+            }
+            callBack(JSON.parse(fileContent));
+        });
+        
     }
 
-}
+};
